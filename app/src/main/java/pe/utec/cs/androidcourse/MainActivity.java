4783cb3646f3f25bde7e8a3f313c7a730b99d690
Luis Talavera <luis@yodopay.com>
2018-12-20 16:43:04 -0500

    Initial commit

---
+++ app/src/main/java/pe/utec/cs/androidcourse/MainActivity.java
@@ -0,0 +1,12 @@
+package pe.utec.cs.androidcourse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
